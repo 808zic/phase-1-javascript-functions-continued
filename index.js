98dc2f57276c77e3dbfce31530e61171467c5498
Georgemugo <georgepmash@gmail.com>
2024-04-17 11:25:41 +0300

    Adds validation

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate", ){
+   return `This Saturday, I want to ${activity}!`;
+      
+    }
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+
+}
+console.log (mondayWork());
+
+function wrapAdjective(flair = "*") {
+    return function(adj = "special") {
+      return `You are ${flair}${adj}${flair}!`;
+    }
+  }
+
+  const encouragingPromptFunction = wrapAdjective("%");
+console.log(encouragingPromptFunction("a dedicated programmer"));
